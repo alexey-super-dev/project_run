522674f7d1500d4c4b6a6aca057f493789d93558
Alexey Kurylev <chepe4pi@gmail.com>
2024-08-02 16:30:38 -0300

    add view hello world

--- project_run/urls.py
+++ project_run/urls.py
@@ -19,6 +19,10 @@ from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
 
+from autos.views import get_autos
+
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns += path('autos/', get_autos, name='autos')
