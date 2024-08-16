4275cac4368ebc77fcefa6055478f885b077a5e7
Alexey Kurylev <chepe4pi@gmail.com>
2024-08-16 16:07:30 -0300

    add task

--- autos/views.py
+++ autos/views.py
@@ -1,5 +1,7 @@
 from django.http import JsonResponse, HttpResponse
-from rest_framework import viewsets
+from rest_framework import viewsets, mixins, status
+from rest_framework.response import Response
+from rest_framework.viewsets import GenericViewSet
 
 from .models import Autos, Run  # Ensure the Autos model is imported
 from .serializers import RunSerializer
@@ -16,6 +18,17 @@ def get_autos_page(request):
     return HttpResponse([f'name is {auto.name}\n' for auto in autos])
 
 
-class RunsViewSet(viewsets.ModelViewSet):
+class RunsViewSet(mixins.CreateModelMixin,
+                  mixins.RetrieveModelMixin,
+                  mixins.UpdateModelMixin,
+                  mixins.DestroyModelMixin,
+                  mixins.ListModelMixin,
+                  GenericViewSet):
     queryset = Run.objects.all()
     serializer_class = RunSerializer
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        1/0
+        return Response(status=status.HTTP_204_NO_CONTENT)
