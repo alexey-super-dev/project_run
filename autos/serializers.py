451ecb978a6b52716faf478bbedfdbd5a0872ef1
Alexey Kurylev <chepe4pi@gmail.com>
2024-08-18 20:33:57 -0300

    change url

--- autos/serializers.py
+++ autos/serializers.py
@@ -4,8 +4,7 @@ from autos.models import Run
 
 
 class RunSerializer(serializers.ModelSerializer):
-    comment2 = serializers.CharField(required=True, write_only=True)
 
     class Meta:
         model = Run
-        fields = ['id', 'runner', 'comment', 'comment2']
+        fields = ['id', 'runner', 'comment']
