2df94626e06e5b8807fd9f3857c4e4889995375a
Alexey Kurylev <chepe4pi@gmail.com>
2024-09-16 15:49:51 -0300

    add url

--- autos/views.py
+++ autos/views.py
@@ -1,5 +1,7 @@
 from django.http import JsonResponse, HttpResponse
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from .models import Autos, Run  # Ensure the Autos model is imported
 from .serializers import RunSerializer
@@ -19,3 +21,14 @@ def get_autos_page(request):
 class RunsViewSet(viewsets.ModelViewSet):
     queryset = Run.objects.filter()
     serializer_class = RunSerializer
+
+    @action(detail=True, methods=['post'], url_path='start')
+    def start_run(self, request, pk=None):
+        try:
+            run = self.get_object()  # Получить объект Run по ID из URL
+        except Run.DoesNotExist:
+            return Response({'error': 'Run not found'}, status=status.HTTP_404_NOT_FOUND)
+            # Ваш код для запуска run
+        run.status = 'go'  # Например, метод start() запускает ваш объект
+        run.save()
+        return Response({'status': 'run started'}, status=status.HTTP_200_OK)
