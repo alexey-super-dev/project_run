2b75d83d904fcf2f5b33f0d70f5c40338a335345
Alexey Kurylev <chepe4pi@gmail.com>
2024-09-16 12:37:39 -0300

    add migration

--- autos/models.py
+++ autos/models.py
@@ -10,3 +10,4 @@ class Run(models.Model):
     comment = models.CharField(max_length=250)
     athlete = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True, null=True)
+    status = models.CharField(max_length=50, default='ok')
