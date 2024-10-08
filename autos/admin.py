97690e8463d16e3c4aae1b2205337abf847d2fab
Alexey Kurylev <chepe4pi@gmail.com>
2024-10-08 15:12:59 -0300

    add admin

--- autos/admin.py
+++ autos/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
-from autos.models import Autos
+from autos.models import Autos, Position, Run
 
 admin.site.register(Autos)
+admin.site.register(Run)
+admin.site.register(Position)
