cf81ab20bd622de3df790543a2795b6e02c0002b
Alexey Kurylev <chepe4pi@gmail.com>
2024-10-28 04:39:43 -0300

    test

--- autos/views.py
+++ autos/views.py
@@ -135,7 +135,7 @@ class UsersViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     filter_backends = [filters.SearchFilter]
-    search_fields = ['first_name', 'last_name']
+    search_fields = ['first_name', 'last_name', 'id']
 
     def get_queryset(self):
         queryset = super().get_queryset()
