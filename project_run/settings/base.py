890c38b4db1848cb1545a22b87b0f5a60b1d2919
Alexey Kurylev <chepe4pi@gmail.com>
2024-11-21 18:51:08 -0300

    error

--- project_run/settings/base.py
+++ project_run/settings/base.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+from pandas import a as b
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -109,4 +109,3 @@ STATIC_ROOT = 'static'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-import a
