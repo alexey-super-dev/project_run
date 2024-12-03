2f21e91f55beb6e03dc660e0869f1f90a464437f
Alexey Kurylev <chepe4pi@gmail.com>
2024-12-02 22:50:56 -0300

    add validation

--- autos/views.py
+++ autos/views.py
@@ -24,7 +24,7 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.views import APIView
 
 from .logic import calculate_run_time_by_id, calculate_run_time, calculate_run_time_different_way, calculate_median, \
-    call_carboninterface
+    call_carboninterface, validate_url
 from .models import Autos, Run, Position, AthleteCoachRelation, ChallengeRecord, \
     AthleteInfo  # Ensure the Autos model is imported
 from .serializers import RunSerializer, PositionSerializer, UserSerializer, DetailAthleteSerializer, \
@@ -741,7 +741,8 @@ class UploadXLSX(APIView):
             # Read the contents
             data = []
             for row in worksheet.iter_rows(values_only=True):
-                if random.choice([True, False]):
+                # if random.choice([True, False]):
+                if type(row[0]) != str or type(row[1]) != int or not (-90 <= int(row[2]) <= 90) or not (-180 <= int(row[3]) <= 180) or not validate_url(row[4]):
                     data.append(row)
 
             # Return the parsed data as JSON
