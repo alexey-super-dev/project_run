0dd3d49d54b19f1d5435a50af298c557ff41dd09
Alexey Kurylev <chepe4pi@gmail.com>
2024-12-24 23:33:40 -0300

    add weight, remove level

--- autos/models.py
+++ autos/models.py
@@ -52,7 +52,7 @@ class ChallengeRecord(models.Model):
 
 class AthleteInfo(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    weight = models.IntegerField(null=True)
+    weight = models.IntegerField(null=True, validators=[MinValueValidator(30), MaxValueValidator(200)])
     goals = models.CharField(max_length=255, default='')
 
 
