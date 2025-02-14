d9adcfa7904c092c1336ffbc5af6e356ceb6597b
Alexey Kurylev <chepe4pi@gmail.com>
2025-02-14 17:16:48 -0300

    add field

--- autos/serializers.py
+++ autos/serializers.py
@@ -46,7 +46,7 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         # fields = ['id', 'username', 'last_name', 'first_name', 'type', 'runs_finished']
-        fields = ['id', 'username', 'last_name', 'first_name', 'type', 'runs_finished', 'rating']
+        fields = ['id', 'username', 'last_name', 'first_name', 'type', 'runs_finished', 'rating', 'date_joined']
 
     def get_rating(selff, obj):
         if hasattr(obj, 'average_rating'):
