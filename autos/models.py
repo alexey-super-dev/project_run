133e7c349fdd05ee186a3900aded7e06509190c7
Alexey Kurylev <chepe4pi@gmail.com>
2025-03-28 12:41:34 -0300

    check

--- autos/models.py
+++ autos/models.py
@@ -53,7 +53,7 @@ class ChallengeRecord(models.Model):
 class AthleteInfo(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     weight = models.IntegerField(null=True, validators=[MinValueValidator(30), MaxValueValidator(200)])
-    goals = models.CharField(max_length=255, default='')
+    goals = models.CharField(max_length=200, default='')
     test_field = models.CharField(max_length=255, default='')
 
 
